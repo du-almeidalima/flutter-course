f2005d9c887599ab8c4ed69ac4e58bc15e558c60
Eduardo Lima <dualmeidalima@gmail.com>
2020-10-26 17:16:36 -0300

    Section 4: Further Input & Output Styling and Configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
-
   final Function _onAddTransaction;
+
   // We can pass TextControllers to TextFields and Flutter will register the
   // Value on them, so we don't need to manually create functions to do so.
   final _itemController = TextEditingController();
   final _amountController = TextEditingController();
 
   NewTransaction(this._onAddTransaction);
+
+  void _submitTransaction() {
+    final item = this._itemController.text;
+    final amount = _amountController.text.isEmpty
+        ? 0
+        : double.parse(this._amountController.text);
+
+    if (item.isEmpty || amount <= 0) {
+      return;
+    }
+
+    this._onAddTransaction(
+        _itemController.text, double.parse(_amountController.text));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +36,20 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Item'),
               controller: this._itemController,
+              onSubmitted: (_) {
+                this._submitTransaction();
+              },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: this._amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) {
+                this._submitTransaction();
+              },
             ),
             OutlineButton(
-              onPressed: () {
-                this._onAddTransaction(
-                  _itemController.text,
-                  double.parse(_amountController.text)
-                );
-              },
+              onPressed: this._submitTransaction,
               child: Text('Save'),
               textColor: Colors.blueAccent,
             )
