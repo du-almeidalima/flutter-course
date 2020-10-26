578b998c5909d4a204336fbca64f744d3c4455d7
Eduardo Lima <dualmeidalima@gmail.com>
2020-10-25 21:55:38 -0300

    Section 4: Making things Scrollabe

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,27 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Meu Dinheirinho'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          // The Text Widget depends on the size of its content and the Card
-          // Will fit the size of its child, unless its parent has a defined
-          // width
-          Card(
-            child: Text('CHART'),
-          ),
-          UserTransactions()
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // The Text Widget depends on the size of its content and the Card
+            // Will fit the size of its child, unless its parent has a defined
+            // width
+            Card(
+              child: Text('CHART'),
+            ),
+            UserTransactions()
+          ],
+        ),
       ),
     );
   }
 }
+
+/*
+ * The Body is wrapped in a SingleChildScrollView, because, whenever the
+ * Soft Keyboard is triggered, it adds the input height as bottom padding
+ * so it's always visible. However, if the heigh is already full, it throws an
+ * error
+ */
--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -9,50 +9,60 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ...this._userTransactions.map((transaction) {
-          return Card(
-            child: Row(children: [
-              Container(
-                child: Text(
-                  "\$ ${transaction.amount.toStringAsFixed(2)}",
-                  softWrap: false,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.lightBlueAccent,
-                  ),
-                ),
-                width: 100,
-                margin:
-                EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border:
-                  Border.all(width: 2, color: Colors.lightBlueAccent),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    transaction.title,
-                    style: TextStyle(
-                      color: Colors.lightBlueAccent[700],
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+    return Container(
+      height: 450,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ...this._userTransactions.map((transaction) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    child: Text(
+                      "\$ ${transaction.amount.toStringAsFixed(2)}",
+                      softWrap: false,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.lightBlueAccent,
+                      ),
+                    ),
+                    width: 100,
+                    margin:
+                    EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border:
+                      Border.all(width: 2, color: Colors.lightBlueAccent),
                     ),
                   ),
-                  Text(DateFormat.yMMMMd().format(transaction.date)),
-                ],
-              ),
-            ]),
-          );
-        }).toList()
-      ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                          color: Colors.lightBlueAccent[700],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(DateFormat.yMMMMd().format(transaction.date)),
+                    ],
+                  ),
+                ]),
+              );
+            }).toList()
+          ],
+        ),
+      ),
     );
   }
 }
+
+/*
+ * In Flutter if we want to make some Widget scrollable, we need to first,
+ * define a height on it, and then we can wrap it SingleChildScrollView
+ */
