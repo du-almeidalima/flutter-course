999da59fac4612841c749fad3bfc04ca7ad8fd98
Eduardo Lima <dualmeidalima@gmail.com>
2020-10-29 18:03:08 -0300

    Section 7: Selecting Meals for a Chosen Category

--- lib/pages/category_meals_page.dart
+++ lib/pages/category_meals_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:meu_rango/mock/meals_data.dart';
 
 class CategoryMealsPage extends StatelessWidget {
   final String id;
   final String categoryTitle;
 
   static const routeName = '/category-meals';
+
   const CategoryMealsPage({this.id, this.categoryTitle});
 
   @override
@@ -13,13 +15,19 @@ class CategoryMealsPage extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categoriesList.any((id) => categoryId == id))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text("heeeey!"),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
