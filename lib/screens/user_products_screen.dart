5e62f40cf9b0d8b0cd7720ff6823a8bb2a12b9e5
Eduardo Lima <dualmeidalima@gmail.com>
2020-11-17 17:13:12 -0300

    Section 9: Implementing Pull-to-Refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -8,6 +8,11 @@ import 'package:shopps/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const String routeName = '/user-products';
 
+  // This method will aways return a Future
+  Future<void> screenRefreshed(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetch();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsProvider = Provider.of<Products>(context);
@@ -24,12 +29,15 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsProvider.products.length,
-          itemBuilder: (ctx, i) =>
-              UserProductItem(productsProvider.products[i]),
+      body: RefreshIndicator(
+        onRefresh: () => this.screenRefreshed(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsProvider.products.length,
+            itemBuilder: (ctx, i) =>
+                UserProductItem(productsProvider.products[i]),
+          ),
         ),
       ),
       drawer: MainDrawer(),
