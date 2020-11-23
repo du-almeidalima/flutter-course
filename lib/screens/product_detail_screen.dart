4ab430a893baa86486b63ac2308a088696b07543
Eduardo Lima <dualmeidalima@gmail.com>
2020-11-23 10:54:32 -0300

    Section 10: Working with Slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -8,8 +8,37 @@ import 'package:shopps/screens/cart_screen.dart';
 import 'package:shopps/utils/GlobalScaffoldKey.dart';
 import 'package:shopps/widgets/badge.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
   static const String routeName = '/product-detail';
+
+  final navBarExpandedHeight = 300.0;
+  final navBarHeight = 50.0;
+
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  ScrollController _scrollController;
+  bool _isExpanded = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    this._scrollController = ScrollController()
+      ..addListener(() {
+        final isExpanded = this._scrollController.hasClients &&
+            this._scrollController.offset <
+                widget.navBarExpandedHeight - 2 * widget.navBarHeight;
+
+        if (this._isExpanded != isExpanded) {
+          setState(() {
+            this._isExpanded = isExpanded;
+          });
+        }
+      });
+  }
 
   void _handleProductFavorite(BuildContext context, String productId) async {
     try {
@@ -35,53 +64,71 @@ class ProductDetailScreen extends StatelessWidget {
     final product = productsProvider.findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-        actions: [
-          Consumer<Cart>(
-            builder: (ctx, cart, child) => Badge(
-              child: child,
-              value: cart.itemCount.toString(),
-            ),
-            child: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {
-                Navigator.of(context).pushNamed(CartScreen.routeName);
-              },
-            ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              // The tag must match the previous Hero tag (product_item.dart)
-              Hero(
+      body: CustomScrollView(
+        // We can attach controllers to a ScrollView to do custom event handling
+        // In this case, I use it to control the isExpanded value
+        controller: this._scrollController,
+        // Slivers are scrollable areas of screen, parts you can scroll
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            actions: [
+              Consumer<Cart>(
+                builder: (ctx, cart, child) => Badge(
+                  child: child,
+                  value: cart.itemCount.toString(),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  },
+                ),
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.zero,
+              title: AnimatedContainer(
+                duration: Duration(milliseconds: 250),
+                width: double.infinity,
+                height: widget.navBarHeight,
+                color: this._isExpanded ? Colors.black54 : Colors.transparent,
+                child: Center(
+                  child: Text(
+                    product.title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: this._isExpanded ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              background: Hero(
                 tag: product.id,
                 child: Image.network(
                   product.imageUrl,
                   width: double.infinity,
                   height: MediaQuery.of(context).size.height * 0.5,
+                  fit: BoxFit.cover,
                 ),
               ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
               SizedBox(height: 20),
               Text(
-                product.title,
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 15),
-              Text(
                 product.description,
+                textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 20),
               ),
-            ],
-          ),
-        ),
+              SizedBox(height: 800),
+            ]),
+          )
+        ],
       ),
       bottomNavigationBar: ButtonBar(
         mainAxisSize: MainAxisSize.max,
