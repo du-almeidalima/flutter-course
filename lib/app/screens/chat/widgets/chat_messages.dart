496b445fa12e4c5a2158b07aa53ec7701ac5fc2a
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2021-01-04 15:09:35 -0300

    Section 14: Create a Basic Chat Bubble Widget

--- lib/app/screens/chat/widgets/chat_messages.dart
+++ lib/app/screens/chat/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_chat/data/chat/entity/ChatMessage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,12 +12,11 @@ class ChatMessages extends StatelessWidget {
       listener: (_, __) => null,
       builder: (context, state) {
         return state.maybeWhen(
-          initial: () => Center(child: Text('No messages yet')),
-          loading: () => Center(child: CircularProgressIndicator()),
-          loaded: (messages) => this._buildChatMessages(context, messages),
-          error: (failure) => Center(child: Text('An error has occurred!')),
-          orElse: () => null
-        );
+            initial: () => Center(child: Text('No messages yet')),
+            loading: () => Center(child: CircularProgressIndicator()),
+            loaded: (messages) => this._buildChatMessages(context, messages),
+            error: (failure) => Center(child: Text('An error has occurred!')),
+            orElse: () => null);
       },
     );
   }
@@ -24,10 +24,49 @@ class ChatMessages extends StatelessWidget {
   Widget _buildChatMessages(BuildContext context, List<dynamic> messages) {
     return ListView.builder(
       reverse: true,
+
       itemCount: messages.length,
       itemBuilder: (context, index) {
-        return Text(messages[index]);
+        return _MessageBubble(
+          chatMessage: ChatMessage(
+            messages[index],
+          ),
+        );
       },
+    );
+  }
+}
+
+class _MessageBubble extends StatelessWidget {
+  final ChatMessage chatMessage;
+
+  const _MessageBubble({Key key, this.chatMessage}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: 100,
+            maxWidth: 250,
+          ),
+          child: Container(
+            margin: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Theme.of(context).primaryColorLight,
+            ),
+            child: Text(
+              chatMessage.content,
+              style: TextStyle(
+                color: Theme.of(context).primaryTextTheme.bodyText1.color,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
